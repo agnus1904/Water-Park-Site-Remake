f8b4451491f554821456d62a7566fc8701493990
khoa <agnus1904@gmail.com>
2020-08-18 03:34:56 +0700

    Add about us section and fix link navbar

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const loader = document.querySelector(".loader");
                 loader.style.opacity = "0";
                 setTimeout(function(){
                     loader.style.display = "none";
-                },500);
-            },1500);
+                },0);
+            },0);
         };
 
         window.addEventListener("scroll", function(){
